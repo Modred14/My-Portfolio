ede1f5e4c9d5e11ea766449bc324f8600ffdde03
Modred14 <favourdomirin@gmail.com>
2024-06-18 07:55:04 -0700

    @media update

--- blog/hello.js
+++ blog/hello.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+document.addEventListener("DOMContentLoaded", function () {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    // Detect Android
-    if (/android/i.test(userAgent)) {
-        document.body.classList.add('android');
-    }
+  // Detect Android
+  if (/android/i.test(userAgent)) {
+    document.body.classList.add("android");
+  }
 
-    // Detect iOS
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        document.body.classList.add('ios');
-    }
+  // Detect iOS
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    document.body.classList.add("ios");
+  }
 });
--- schema.js
+++ schema.js
@@ -1,38 +1,38 @@
-
-const personData= {
-    "@context": "https://schema.org",
-      "@type": "Person",
-      "name": "Favour Omirin",
-      "url": "https://favouromirin.dev",
-      "image": "https://favouromirin.dev/image.jpg",
-      "jobTitle": "Frontend Engineer",
-      "description": "👩‍💻👨‍💻Frontend software engineer skilled in JavaScript, CSS, 💻HTML, user-centric interfaces",
-      "email": "favourdomirin@gmail.com",
-      "telephone": "+2348149488987",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "Oniyanrin",
-        "addressLocality": "Ile-Ife",
-        "addressRegion": "Osun State",
-        "postalCode": "220231",
-        "addressCountry": "Nigeria"
-      },
-      "alumniOf": {
-        "@type": "CollegeOrUniversity",
-        "name": "Faith Standard Cephalo Model College",
-        "url": "https://faithstandardschools.net"
-      },
-      "birthDate": "2008-04-14",
-      "gender": "Male",
-      "nationality": "Nigerian",
-      "sameAs": [
-        "https://github.com/Modred14",
-        "https://x.com/omirin_favour",
-        "https://www.linkedin.com/in/omirin-favour",
-        "https://www.facebook.com/favour.omirin7",
-        "https://www.instagram.com/omirinfavour",
-        "https://leetcode.com/Modred14",
-        "https://favouromirin.dev",
-      ]
-    }
-    console.log(personData)
+const personData = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Favour Omirin",
+  url: "https://favouromirin.dev",
+  image: "https://favouromirin.dev/image.jpg",
+  jobTitle: "Frontend Engineer",
+  description:
+    "👩‍💻👨‍💻Frontend software engineer skilled in JavaScript, CSS, 💻HTML, user-centric interfaces",
+  email: "favourdomirin@gmail.com",
+  telephone: "+2348149488987",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Oniyanrin",
+    addressLocality: "Ile-Ife",
+    addressRegion: "Osun State",
+    postalCode: "220231",
+    addressCountry: "Nigeria",
+  },
+  alumniOf: {
+    "@type": "CollegeOrUniversity",
+    name: "Faith Standard Cephalo Model College",
+    url: "https://faithstandardschools.net",
+  },
+  birthDate: "2008-04-14",
+  gender: "Male",
+  nationality: "Nigerian",
+  sameAs: [
+    "https://github.com/Modred14",
+    "https://x.com/omirin_favour",
+    "https://www.linkedin.com/in/omirin-favour",
+    "https://www.facebook.com/favour.omirin7",
+    "https://www.instagram.com/omirinfavour",
+    "https://leetcode.com/Modred14",
+    "https://favouromirin.dev",
+  ],
+};
+console.log(personData);
