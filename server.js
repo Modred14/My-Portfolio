a35c7fdcb765df880550162d8d9a2ae61050a32f
Modred14 <favourdomirin@gmail.com>
2024-06-21 09:28:42 -0700

    Comment section update

--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 const app = express();
+const PORT = 3000;
+
 app.use(bodyParser.json());
 app.use(cors());
 
-const dbUrl = process.env.DB_URL; 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+let comments = [];
 
-
-const commentSchema = new mongoose.Schema({
-  name: String,
-  text: String,
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
 
-const Comment = mongoose.model('Comment', commentSchema);
-
-// API endpoints
-app.get('/comments', async (req, res) => {
-  const comments = await Comment.find();
-  res.send(comments);
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  res.status(201).json(comment);
 });
 
-app.post('/comments', async (req, res) => {
-  const { name, text } = req.body;
-  const newComment = new Comment({ name, text });
-  await newComment.save();
-  res.send(newComment);
+app.delete('/comments/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+  if (index >= 0 && index < comments.length) {
+    comments.splice(index, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Comment not found');
+  }
 });
 
-app.delete('/comments/:id', async (req, res) => {
-  await Comment.findByIdAndDelete(req.params.id);
-  res.send({ message: 'Comment deleted' });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
